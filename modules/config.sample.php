75a10c522e160ae743183dd12228286673d1a622
Nico Fricke <nico@nifni.net>
2018-04-15 22:41:25 +0200

    added some documentation to the config

--- modules/config.sample.php
+++ modules/config.sample.php
@@ -3,33 +3,29 @@
 // FIRST USE
 // Please copy this file as config.php
 
-// To edit a config uncomment the line, otherwise
-// defaults will be used for each variable. 
-
 // ----------- General Variables -----------
 
 // Name of your node (default: your hostname)
 $nanoNodeName = 'nano.nifni.net';
 
-// choice of Nano block explorer ('nanode', 'nanoexplorer', 'nano')
-// $blockExplorer = 'nanode';
-
 // ----------- Nano Node Variables -----------
 
-// ip address for RPC (default: [::1])
-$nanoNodeRPCIP   = 'nano.nifni.net';
+// ip address for the node RPC
+$nanoNodeRPCIP   = '[::1]';
 
-// ip address for RPC (default: 7076)
-$nanoNodeRPCPort = '443';
+// ip address for RPC
+$nanoNodeRPCPort = '7076';
 
-$nanoNodeRPCPath = '/node';
+// adds a path to the url where your node rpc is accessed by. Is only relevant when having a reverse proxy in front of your node.
+// In most cases this can just be left empty.
+$nanoNodeRPCPath = '';
 
 // account of this node
 $nanoNodeAccount = 'xrb_1fnx59bqpx11s1yn7i5hba3ot5no4ypy971zbkp5wtium3yyafpwhhwkq8fc';
 
-// donation account for maintaining this node
+// donation account
 $nanoDonationAccount = 'xrb_1xgfouwac4kd5d9bwsf18jp53g3sezcczoqxcfge1efwbntnjwrdky1jto4k';
 
-// number of decimal places to display Nano balances, i.e.
+// number of decimal places to display Nano balances
 $nanoNumDecimalPlaces = 2;
 
